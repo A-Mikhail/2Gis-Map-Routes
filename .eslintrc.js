bfb9e74f48749802dc00c3ef53cce0ccc216a7e2
Mikhail Adamenko <mikhail.adamenko@protonmail.com>
2018-10-03 15:44:55 +0600

    Added Custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "indent": 0,
+        "react/jsx-indent": 0,
+        "react/jsx-indent-props": 0,
+        "react/no-multi-comp": 0,
+    }
 };
