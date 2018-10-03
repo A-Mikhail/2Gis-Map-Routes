51d353db05daa89dfbbd1c9f3c04c3d0dc4948b2
Mikhail Adamenko <mikhail.adamenko@protonmail.com>
2018-10-03 15:48:35 +0600

    Refactor componentDidMount()

--- src/Map/index.js
+++ src/Map/index.js
@@ -4,7 +4,7 @@ import { Notification } from '../Controllers/Notification';
 
 const DG = require('2gis-maps');
 
-class Map extends Component {
+export default class Map extends Component {
   constructor(props) {
     super(props);
 
@@ -37,9 +37,17 @@ class Map extends Component {
       maxZoom: this.props.maxZoom,
     });
 
-    if (this.props.admin) {
-      let that = this;
+    this.mapControl(createdMap);
+    this.mapDraw(createdMap);
+    this.mapConvert(createdMap);
 
+    if (this.props.mapPath) {
+      this.importMap(createdMap);
+    }
+  }
+
+  mapControl(map) {
+    if (this.props.admin) {
       // define toolbar options
       const options = {
         position: 'topleft', // toolbar position, options are 'topleft', 'topright', 'bottomleft', 'bottomright'
@@ -54,121 +62,109 @@ class Map extends Component {
       };
 
       // add leaflet.pm controls to the map
-      createdMap.pm.addControls(options);
-
-      // listen to drawing mode gets enabled
-      createdMap.on('pm:drawstart', (e) => {
-        const layer = e.workingLayer;
-
-        // Listen new added vertex (Line & Poly only)
-        layer.on('pm:vertexadded', (e) => {
-          that.setState(previousState => ({
-            shapeType: [...previousState.shapeType, e.shape],
-          }));
-
-          const lastUsedShape = that.state.shapeType[that.state.shapeType.length - 1];
-
-          if (e.shape && lastUsedShape === 'Line') {
-            const newCoordinates = [e.latlng.lng, e.latlng.lat];
-
-            that.setState(previousState => ({
-              shapeType: e.shape,
-              coordinates: [...previousState.coordinates, newCoordinates],
-            }));
-          } else if (e.shape && lastUsedShape === 'Poly') {
-            const newCoordinates = [e.latlng.lng, e.latlng.lat];
-
-            that.setState(previousState => ({
-              shapeType: e.shape,
-              coordinates: [...previousState.coordinates, newCoordinates],
-            }));
-          }
-        });
-
-        layer.pm.hasSelfIntersection();
-      });
-
-      // listen to when a new layer is created
-      createdMap.on('pm:create', (e) => {
-        if (e.shape === 'Marker') {
-          const newCoordinates = [e.layer._latlng.lng, e.layer._latlng.lat];
-
-          const geoJsonFeature = {
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'Point',
-              coordinates: newCoordinates,
-            },
-          };
-
-          that.setState(previousState => ({
-            featuresArray: [...previousState.featuresArray, geoJsonFeature],
-            coordinates: [], // clear coordinates after writing to the featuresArray
-          }));
-        }
-      });
-
-      // Convert leaflet.pm data to geoJson
-      createdMap.on('pm:drawend', (e) => {
-        if (e.shape === 'Line') {
-          // Make LineString out of Line's shape coordinates
-          const geoJsonFeature = {
-            type: 'Feature',
-            properties: null,
-            geometry: {
-              type: 'LineString',
-              coordinates: that.state.coordinates,
-            },
-          };
-
-          that.setState(previousState => ({
-            featuresArray: [...previousState.featuresArray, geoJsonFeature],
-            coordinates: [], // clear coordinates after writing to the featuresArray
-          }));
-        } else if (e.shape === 'Poly') {
-          // Take the last coordinate of the Polygon
-          // and push it to the first element of the array
-          const lastCoordinate = that.state.coordinates[that.state.coordinates.length - 1];
-          that.state.coordinates.unshift(lastCoordinate);
-
-          // Make Polygon out of Poly shapes
-          const geoJsonFeature = {
-            type: 'Feature',
-            properties: null,
-            geometry: {
-              type: 'Polygon',
-              coordinates: [that.state.coordinates],
-            },
-          };
-
-          that.setState(previousState => ({
-            featuresArray: [...previousState.featuresArray, geoJsonFeature],
-            coordinates: [], // clear coordinates after writing to the featuresArray
-          }));
-        }
-      });
+      map.pm.addControls(options);
     }
-
-    this.importMap(createdMap);
   }
 
-  // get information from the file
-  importMap(map) {
-    const onChange = (event) => {
-      const reader = new FileReader();
-      reader.onload = onReaderLoad;
-      reader.readAsText(event.target.files[0]);
-    };
+  mapDraw(map) {
+    const that = this;
 
-    const onReaderLoad = event => {
-      const obj = JSON.parse(event.target.result);
+    // listen to drawing mode gets enabled
+    map.on('pm:drawstart', (e) => {
+      const layer = e.workingLayer;
 
-      // Add our geoJson data to the map
-      let polygonLayer = DG.geoJson(obj).addTo(map);
-    };
+      // Listen new added vertex (Line & Poly only)
+      layer.on('pm:vertexadded', (e) => {
+        that.setState(previousState => ({
+          shapeType: [...previousState.shapeType, e.shape],
+        }));
 
-    document.getElementById('file').addEventListener('change', onChange);
+        const lastUsedShape = that.state.shapeType[that.state.shapeType.length - 1];
+
+        if (e.shape && lastUsedShape === 'Line') {
+          const newCoordinates = [e.latlng.lng, e.latlng.lat];
+
+          that.setState(previousState => ({
+            shapeType: e.shape,
+            coordinates: [...previousState.coordinates, newCoordinates],
+          }));
+        } else if (e.shape && lastUsedShape === 'Poly') {
+          const newCoordinates = [e.latlng.lng, e.latlng.lat];
+
+          that.setState(previousState => ({
+            shapeType: e.shape,
+            coordinates: [...previousState.coordinates, newCoordinates],
+          }));
+        }
+      });
+
+      layer.pm.hasSelfIntersection();
+    });
+
+    // listen to when a new layer is created
+    map.on('pm:create', (e) => {
+      if (e.shape === 'Marker') {
+        const newCoordinates = [e.layer._latlng.lng, e.layer._latlng.lat];
+
+        const geoJsonFeature = {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: newCoordinates,
+          },
+        };
+
+        that.setState(previousState => ({
+          featuresArray: [...previousState.featuresArray, geoJsonFeature],
+          coordinates: [], // clear coordinates after writing to the featuresArray
+        }));
+      }
+    });
+  }
+
+  mapConvert(map) {
+    const that = this;
+
+    // Convert leaflet.pm data to geoJson
+    map.on('pm:drawend', (e) => {
+      if (e.shape === 'Line') {
+        // Make LineString out of Line's shape coordinates
+        const geoJsonFeature = {
+          type: 'Feature',
+          properties: null,
+          geometry: {
+            type: 'LineString',
+            coordinates: that.state.coordinates,
+          },
+        };
+
+        that.setState(previousState => ({
+          featuresArray: [...previousState.featuresArray, geoJsonFeature],
+          coordinates: [], // clear coordinates after writing to the featuresArray
+        }));
+      } else if (e.shape === 'Poly') {
+        // Take the last coordinate of the Polygon
+        // and push it to the first element of the array
+        const lastCoordinate = that.state.coordinates[that.state.coordinates.length - 1];
+        that.state.coordinates.unshift(lastCoordinate);
+
+        // Make Polygon out of Poly shapes
+        const geoJsonFeature = {
+          type: 'Feature',
+          properties: null,
+          geometry: {
+            type: 'Polygon',
+            coordinates: [that.state.coordinates],
+          },
+        };
+
+        that.setState(previousState => ({
+          featuresArray: [...previousState.featuresArray, geoJsonFeature],
+          coordinates: [], // clear coordinates after writing to the featuresArray
+        }));
+      }
+    });
   }
 
   exportMap() {
@@ -191,18 +187,32 @@ class Map extends Component {
     downloadAnchorNode.remove();
   }
 
-  render() {
-    let expMapButton;
-    const impMapButton = <input id="file" type="file" />;
+  // Import map used assigned path from the <Map /> props
+  // to immediately display it on the map
+  // as only its found the geoJson file
+  importMap(map) {
+    const obj = new XMLHttpRequest();
+    obj.overrideMimeType('application/json');
+    obj.open('GET', this.props.mapPath, true);
 
+    obj.onreadystatechange = () => {
+      const parsedRoute = JSON.parse(obj.responseText);
+
+      DG.geoJson(parsedRoute).addTo(map);
+    };
+
+    obj.send(null);
+  }
+
+  render() {
+    // Admin mode
     if (this.props.admin) {
-      expMapButton = <button type="button" onClick={this.exportMap} className="button is-primary"> Сохранить карту </button>;
+      const expMapButton = <button type="button" onClick={this.exportMap} className="button is-primary"> Сохранить карту </button>;
 
       return (
         <>
           <p> Импортировать Карту </p>
 
-          {impMapButton}
           {expMapButton}
 
           <div id={this.state.containerID} className={this.props.className} />
@@ -210,14 +220,11 @@ class Map extends Component {
       );
     }
 
+    // Default user mode
     return (
       <>
-        {impMapButton}
-
         <div id={this.state.containerID} className={this.props.className} />
       </>
     );
   }
 }
-
-export default Map;
